93ea14cd9dbf2dfe733f5116dfc2aa8b0a92b4dc
Halil Yavuz Çevik <yvzcevik@gmail.com>
2018-12-20 15:24:45 +0300

    Update recursiveTrees.sh

--- LinuxShell/Bash/recursiveTrees.sh
+++ LinuxShell/Bash/recursiveTrees.sh
@@ -13,13 +13,25 @@ length=${recToLen[$(( treeCount -1 ))]}
 rootCount=$(( (32 / $length) - 1))
 num_rows=63
 num_columns=100
-for ((i=1;i<=rootCount;i++)) do
+iteration=1
+rootNumber=1
+while [ $iteration -le $treeCount ]
+do
+    for ((i=1;i<=$(( 2 ** (iteration - 1) ));i++)) do
     #Calculating all coordinates of roots
-    rootArrayX[$i]=
-    rootArrayY[$i]=
+     rootArrayX[$rootNumber]=
+     rootArrayY[$rootNumber]=
+     #control echos
+     #echo "iteration number $iteration"
+     #echo "root number $rootNumber"
+     (( rootNumber++ ))
+    done
+    (( iteration++ ))
+    echo
 done
 for ((i=1;i<=num_rows;i++)) do
     for ((j=1;j<=num_columns;j++)) do
         matrix[$i,$j]="-"
     done
 done
+
