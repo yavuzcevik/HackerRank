7afae51d74836cd3df1b77a65b78243f2d1607d3
Halil Yavuz Çevik <yvzcevik@gmail.com>
2019-03-05 15:58:06 +0300

    Update Alphabet Rangoli.py

--- Python/Strings/Alphabet Rangoli.py
+++ Python/Strings/Alphabet Rangoli.py
@@ -1,4 +1,8 @@
+print("Please insert the size of the Rangoli")
 s = "abcdefghijklmnopqrstuvwxyz"
-length = int(input())-1
-for i in range(length+1):
-    print("-".join(s[length:length-i:-1] + s[length-i:length+1]).center(4 * (length+1) - 3, '-'))
+length = int(input()) - 1
+for i in range(2 * length  + 1):
+    if i < length + 1:
+        print("-".join(s[length:length - i:-1] + s[length - i:length + 1]).center(4 * (length + 1) - 3, '-'))
+    if  i >= length+1:
+        print("-".join(s[length:i - length:-1] + s[i - length:length + 1]).center(4 * (length + 1) - 3, '-'))
